4a6f76ebb2bda88209316cf42eeb43cbd2446ce7
heykarthikwithu <heykarthikwithu@gmail.com>
2016-11-04 16:22:49 +0530

    T46 change in travis..

--- scripts/ssh-authenticate.sh
+++ scripts/ssh-authenticate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ssh-keygen -t rsa -N '' -f id_rsa
 ls
-printf 'yes' | ssh-copy-id -i id_rsa.pub d7srijan.dev@free-6101.devcloud.hosting.acquia.com
+ssh-copy-id -i id_rsa.pub d7srijan.dev@free-6101.devcloud.hosting.acquia.com
